0a5d12744b51dc3ce7d385cb0fa7849cd8d68462
Alain Bonneau <alaindev97@gmail.com>
2025-03-28 03:04:16 +0100

    init backend

---
+++ blog-backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema.js";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("Connecté à MongoDB");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(
+    `Serveur GraphQL en cours d'exécution sur http://localhost:${PORT}/graphql`
+  );
+});
