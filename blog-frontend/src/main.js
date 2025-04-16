9b3c381e8d211ecd96cf5dc7bf555d8f1471d852
Alain Bonneau <alaindev97@gmail.com>
2025-04-16 02:52:28 +0200

    add auth js

--- blog-frontend/src/main.js
+++ blog-frontend/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./style.css";
 import router from "./router";
+
+const pinia = createPinia();
+App.use(pinia);
 
 createApp(App).use(router).mount("#app");
