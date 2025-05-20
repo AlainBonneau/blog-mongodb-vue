4063ae048b808b8190a14ca0853288be19841b9c
Alain Bonneau <alaindev97@gmail.com>
2025-05-20 13:49:34 +0200

    correct email validator bug

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -9,7 +9,7 @@ import cors from "cors";
 dotenv.config();
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST"],
 };
